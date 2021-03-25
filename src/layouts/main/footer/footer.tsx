8cedc91eea50d5882b99ff5c3cc107d9c0ecf52f
s.gromov <zz-gromov@ya.ru>
2021-03-26 00:56:43 +0300

    sync

--- src/layouts/main/footer/footer.tsx
+++ src/layouts/main/footer/footer.tsx
@@ -1,16 +1,39 @@
 import React, { FC } from 'react'
 import styled from '@emotion/styled'
+import { Logo } from '../logo'
+import { Navigation } from '../Navigation'
 
 interface FooterProps {}
 
 const _Footer: FC<FooterProps> = () => {
   return (
     <Footer>
-      
+      <Left>
+        <Logo />
+        <Navigation links={[
+          {name: 'Новости', url: '/news'},
+          {name: 'События', url: '/events'},
+          {name: 'Обучения', url: '/training'},
+          {name: 'Работа', url: '/jobs'}
+        ]}/>
+      </Left>
+      <Right>
+        <SocialsTitle>Наши соц сети</SocialsTitle>
+        <Socials>
+          
+        </Socials>
+      </Right>
     </Footer>
   )
 }
 
-const Footer = styled.div``
+const Footer = styled.div`
+  background: #292F3D;
+  display: flex;
+`
+const Left = styled.div``
+const Right = styled.div``
+const SocialsTitle = styled.div``
+const Socials= styled.div``
 
 export { _Footer as Footer }
--- src/layouts/main/main.tsx
+++ src/layouts/main/main.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled'
 import React, { FC } from 'react'
+import { Footer } from './footer'
 import { Header } from './header'
 
 interface MainProps {}
@@ -8,10 +9,13 @@ const _Main: FC<MainProps> = () => {
   return (
     <Main>
       <Header />
+      <Content />
+      <Footer />
     </Main>
   )
 }
 
 const Main = styled.div``
+const Content = styled.div``
 
 export { _Main as MainLayout }
